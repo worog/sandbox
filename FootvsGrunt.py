7010051b6ab5aa440cf24bbff3cba9880872a02d
worog <worog@users.noreply.github.com>
2021-03-11 15:33:20 +0100

    Update FootvsGrunt.py

--- FootvsGrunt.py
+++ FootvsGrunt.py
@@ -2,27 +2,14 @@
 """
 Created on Sun Oct 23 22:11:03 2016
 Footmen vs Grunt
-@author: cube
+@author: Wojciech Rogala
 """
 
 import scipy as sci
 import matplotlib.pyplot as plt
 import numpy as np
 
-Foot=3
-AttF=12.5
-dtF=1.35
-defF=2
-Footred=defF*(0.06)/(1+0.06*defF)
-HpFoot=420
 
-
-Grunt=2
-AttG=19.5
-dtG=1.6
-defG=1
-Gruntred=defG*(0.06)/(1+0.06*defG)
-HpGrunt=700
 
 def GruntsAtt(Footred,Grunt,AttG):
     AnG=(1-Footred)*Grunt*AttG
@@ -40,37 +27,53 @@ def FootsHP(Foot,HpFoot):
     HpnF=[Foot*HpFoot]
     return HpnF
 
+if __name__ == "__main__":
+    Foot=3
+    AttF=12.5
+    dtF=1.35
+    defF=2  
+    Footred=defF*(0.06)/(1+0.06*defF)
+    HpFoot=420
 
-time= np.linspace(0,200,4000)
-AF=[FootsAtt(Gruntred,Foot,AttF)]
-HpF=[FootsHP(Foot,HpFoot)]
-AG=[GruntsAtt(Footred,Grunt,AttG)]
-HpG=[GruntsHP(Grunt,HpGrunt)]
 
-nF=[]
-nG=[]
+    Grunt=2
+    AttG=19.5
+    dtG=1.6
+    defG=1
+    Gruntred=defG*(0.06)/(1+0.06*defG)
+    HpGrunt=700
 
-for i in time:
-  #  while Grunt!=0 || Foot!   
-     nF.append(i%dtF)
-     nG.append(i%dtG)
-       if i%dtF = 0:
+
+    time= np.linspace(0,200,4000)
+    AF=[FootsAtt(Gruntred,Foot,AttF)]
+    HpF=[FootsHP(Foot,HpFoot)]
+    AG=[GruntsAtt(Footred,Grunt,AttG)]
+    HpG=[GruntsHP(Grunt,HpGrunt)]
+
+    nF=[]
+    nG=[]
+
+    for i in time:
+    #  while Grunt!=0 || Foot!   
+    nF.append(i%dtF)
+    nG.append(i%dtG)
+    if i%dtF = 0:
         loss=HpG[i-1]-AF[i-1]
         HpG.append(loss)           
         if HpG[i]%HpGrunt=0:
             Grunt=-1
         AG.append(GruntsAtt(Footred,Grunt,AttG)
-      elif i%dtG=0:
-          loss=HpF[i-1]-AG[i-1]
-          HpG.append(loss)           
-          if HpF[i]%HpFoot=0:
-              Foot=-1 
+    elif i%dtG=0:
+        loss=HpF[i-1]-AG[i-1]
+        HpG.append(loss)           
+        if HpF[i]%HpFoot=0:
+            Foot=-1 
         Ag.append(GruntsAtt(Footred,Grunt,AttG))        
-      else:
+    else:
         HpG.append(HpG[i-1])
         HpF.append(HpF[i-1])
         AG.append(AG[i-1])           
         AF.append(AF[i-1])
            
-plt.plot(AF.time,'r.')
-plt.show()
+    plt.plot(AF.time,'r.')
+    plt.show()
